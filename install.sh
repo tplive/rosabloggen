1809c58c0bad0600a781b1afc1838963ee7e2513
Thomas Qvidahl <thomas.qvidahl@evry.com>
2017-04-25 22:09:06 +0200

    Restart

--- install.sh
+++
@@ -1,5 +0,0 @@
-# How to set up the app
-
-npm install -g gulp
-npm install --save-dev gulp-data gulp-jade gulp-sass gulp-concat gulp-jshint jshint-stylish
-npm install --save mongodb express-session express
